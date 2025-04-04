84596a9461b48e50ed8c0fd02c6dcd6bea29c5a2
Louis Becue <louis.becue.etu@univ-lille.fr>
2025-04-04 14:55:13 +0200

    add the DateFormating strategy

---
+++ gson/src/main/java/com/google/gson/DateFormattingStrategy.java
@@ -0,0 +1,42 @@
+package com.google.gson;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * A simple way to format dates in Gson.
+ */
+public interface DateFormattingStrategy {
+  /**
+   * Convert a date to text.
+   *
+   * @param date the date to change
+   * @return the text version of the date
+   */
+  String format(Date date);
+
+  /**
+   * Change text to a date.
+   *
+   * @param dateStr the text to change
+   * @return the date
+   * @throws ParseException if the text is not correct
+   */
+  Date parse(String dateStr) throws ParseException;
+
+  /**
+   * Check if this strategy supports the date type.
+   *
+   * @param dateType the date type to check
+   * @return true if supported
+   */
+  boolean canHandle(Class<?> dateType);
+
+  /**
+   * Create a type adapter factory for the date.
+   *
+   * @param dateType the date type to use
+   * @return the type adapter factory
+   */
+  TypeAdapterFactory createAdapterFactory(Class<? extends Date> dateType);
+}
